a161c356bbc471791ddad3fbf5a1d9c3b516b57a
vamshi Amudala <vamshipatel126@gmail.com>
2025-06-29 15:42:56 +0530

    minor changes

--- Backend/src/main/java/com/example/MentalHealthCompanionApplication.java
+++ Backend/src/main/java/com/example/MentalHealthCompanionApplication.java
@@ -2,12 +2,24 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MentalHealthCompanionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MentalHealthCompanionApplication.class, args);
-	}
+    static {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
+   
+        System.setProperty("JDBC_URL", dotenv.get("JDBC_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+       
+        System.setProperty("OPENAI_API_KEY", dotenv.get("OPENAI_API_KEY"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentalHealthCompanionApplication.class, args);
+    }
 }
